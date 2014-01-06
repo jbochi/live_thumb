02bf7010b8edfb25703c16d53121f4f8bed16ee9
Juarez Bochi <jbochi@gmail.com>
2014-01-06 14:43:49 -0200

    Assert content is received

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,21 @@
+from geventhttpclient import HTTPClient, URL
+from multiprocessing.pool import ThreadPool
 import os
 import subprocess
 import tempfile
 import time
+
+
+def get_async():
+    pool = ThreadPool(processes=1)
+    async_result = pool.apply_async(get)
+    return async_result
+
+def get():
+    url = URL("http://localhost:9080/sub/channel")
+    http = HTTPClient.from_url(url)
+    response = http.get(url.path)
+    return response
 
 def run_broadcaster(frames_path):
     env = os.environ.copy()
@@ -10,8 +24,10 @@ def run_broadcaster(frames_path):
     time.sleep(1)
     return p
 
-def create_image(tempdir, content):
-    image = tempfile.NamedTemporaryFile(dir=tempdir, delete=False)
+def create_image(tempdir, channel, content):
+    image_dir = os.path.join(tempdir, channel)
+    os.mkdir(image_dir)
+    image = tempfile.NamedTemporaryFile(dir=image_dir, delete=False)
     image.write(content)
     image.close()
     time.sleep(1)
@@ -20,6 +36,15 @@ def create_image(tempdir, content):
 def test_when_file_is_created_its_contents_are_posted():
     tempdir = tempfile.mkdtemp()
     p = run_broadcaster(tempdir)
-    image = create_image(tempdir, "JPEG IMAGE")
+    r = get_async()
+    create_image(tempdir, "channel", "JPEG IMAGE")
+    p.terminate()
+    buffer_size = 40
+    assert "JPEG IMAGE" in r.get(timeout=2).read(buffer_size)
+
+def test_file_is_deleted_after_post():
+    tempdir = tempfile.mkdtemp()
+    p = run_broadcaster(tempdir)
+    image = create_image(tempdir, "channel", "JPEG IMAGE\n")
     p.terminate()
     assert os.path.exists(image.name) is False
