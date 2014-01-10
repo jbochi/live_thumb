d025b102f8813b9fbcbfae6bcaa225df672fc782
Juarez Bochi <jbochi@gmail.com>
2014-01-10 13:58:04 -0200

    Bump up to v0.0.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='MJPEG broadcaster',
     long_description=README,
     author='Globo.com',
-    version='0.0.6',
+    version='0.0.7',
     include_package_data=True,
     install_requires=install_reqs,
     py_modules=[
